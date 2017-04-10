0a3a1b99506d9ca0e74c91f589c1ae7e3dafdf66
Kevin Navero <knavero@gmail.com>
2017-04-10 00:26:46 -0700

    Added list of heroes with badges
    styles.css enhanced to style list elements in template.html.
    10 heroes added to list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,25 +10,35 @@ export class Hero {
   }
 }
 
+const HEROES = [
+  new Hero(11, 'Mr. Nice'),
+  new Hero(12, 'Narco'),
+  new Hero(13, 'Bombasto'),
+  new Hero(14, 'Celeritas'),
+  new Hero(15, 'Magneta'),
+  new Hero(16, 'Rubberman'),
+  new Hero(17, 'Dynama'),
+  new Hero(18, 'Dr IQ'),
+  new Hero(19, 'Magma'),
+  new Hero(20, 'Tornado')
+];
+
 @Component({
   selector: 'my-app',
   // template: `<h1>Hello {{name}}</h1>`, // use backticks to make multiline text
-  templateUrl: 'template.html'
+  templateUrl: 'template.html',
+  styleUrls: ['styles.css']
 })
 export class AppComponent  { 
   title = 'Tour of Heroes';
-  heroes = [
-    new Hero(1, 'Windstorm'), 
-    new Hero(2, 'Bombasto'), 
-    new Hero(3, 'Magneta'), 
-    new Hero(4, 'Tornado')
-  ];  
-  hero: Hero = this.heroes[0];
+  heroes = HEROES;
+  hero = this.heroes[0];
+  many = 5;
 
   // if you need to declare a property of function type consider the syntax below
   // is_many: ()=>Boolean; 
 
-  is_many = function(): Boolean { return this.heroes.length > 2 };
+  is_many = function(): Boolean { return this.heroes.length > this.many };
   name_change = function(event: String): void { 
     console.log("logged " + event + " at " + new Date());
     this.heroes[0].name = event; 
