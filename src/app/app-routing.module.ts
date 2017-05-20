c22b4e79f32667c7fd6e5fa1338b28d0d0db0e5c
Kevin Navero <knavero@gmail.com>
2017-05-20 06:42:18 -0700

    routing separated out to its own module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Route, Routes } from '@angular/router';
+
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+const routes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard', // if prefixed with a slash, this represents
+                              // an absolute redirect as opposed to a relative
+                              // redirect. An absolute redirect replaces the
+                              // entire suffix of the URL, not just what 
+                              // matches in 'path'
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-import { RouterModule, Route, Routes } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { UnlessDirective } from './unless.directive'
@@ -9,36 +8,13 @@ import { HeroDetailComponent } from './hero-detail.component'
 import { HeroesComponent } from './heroes.component'
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
-
-const routes: Routes = [
-  {
-    path: 'heroes',
-    component: HeroesComponent
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },
-  {
-    path: 'detail/:id',
-    component: HeroDetailComponent
-  },
-  {
-    path: '',
-    redirectTo: '/dashboard', // if prefixed with a slash, this represents
-                              // an absolute redirect as opposed to a relative
-                              // redirect. An absolute redirect replaces the
-                              // entire suffix of the URL, not just what 
-                              // matches in 'path'
-    pathMatch: 'full'
-  }
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [ 
     BrowserModule, 
     FormsModule,
-    RouterModule.forRoot(routes) 
+    AppRoutingModule
   ],
   declarations: [ 
     AppComponent, 
