377328cccc4f00c758a5b2a3f41db519ea94d82f
Kevin Navero <knavero@gmail.com>
2017-06-24 19:48:07 -0700

    hero.ts: cleanup

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -2,9 +2,12 @@ export class Hero {
   // non-shorthand (public by default)
   // public id: number 
   // public name: string
+
+  // params with access specifiers become properties 
+  // constructor(public id: number, public name: string) { 
+  //   this.id = id; // assignment here is optional
+  //   this.name = name;
+  // }
   
-  constructor(public id: number, public name: string) { // shorthand
-    this.id = id;
-    this.name = name;
-  }
+  constructor(public id: number, public name: string) {}
 }
